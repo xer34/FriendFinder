5681e52b4d951ab3f6e3ecf8f9d38c9c464f60c9
Xer34 <experimental@gmail.com>
2018-11-09 22:34:14 -0500

    had to restructure things in order to test functionality, will break it shortly

--- app/routing/htmlRoutes.js
+++
@@ -1,7 +0,0 @@
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-  });
--- server.js
+++ server.js
@@ -1,7 +1,83 @@
+// dependencies
 const express = require("express");
+const app = express();
 const path = require("path");
+const PORT = 3000;
+//app use
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+//routes -- I tried to get these into their own files and reference those files, but could not get the __dirname correct? 
+// ../app/public/home.html ... ??????
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/app/public/home.html"));
+});
 
-const routes = require("./app/routing/htmlRoutes")
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
 
 
+app.get("/api/friends", function(req, res) {
+  return res.json(friends);
+});
 
+app.post("/api/friends", function(req, res) {
+  var friends = req.body;
+
+  waitlist.push(friends);
+
+  res.json(friends);
+});
+// again, going to fix pathing and put this into its own file when I get to it
+var friends = [
+  {
+      "name":"TestGuy",
+      "photo":"https://images.fineartamerica.com/images-medium-large-5/jack-skellington-movie-prints.jpg",
+      "scores":[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+    }, {
+      "name":"TestGuy2",
+      "photo":"https://images.fineartamerica.com/images-medium-large-5/jack-skellington-movie-prints.jpg",
+      "scores":[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+    },{
+      "name":"TestGuy3",
+      "photo":"https://images.fineartamerica.com/images-medium-large-5/jack-skellington-movie-prints.jpg",
+      "scores":[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+    },
+  ]
